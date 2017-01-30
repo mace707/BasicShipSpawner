065f6f0dbbeee310bdb6fc6dfd45a0d6ef0af262
Mace <mason@gls.co.za>
2017-01-30 15:37:50 +0200

    Initial Commit

---
+++ EnemySpawnTester/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+	float XMax = 0;
+	float XMin = 0;
+
+	bool MovingRight = true;
+
+	bool MoveDownStep = false;
+
+	private int spawnCount = 50;
+
+	public int GetSpawnCount()
+	{
+		return spawnCount;
+	}
+
+	void Start()
+	{
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		XMin = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceToCamera)).x;
+		XMax = Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0, distanceToCamera)).x;
+	}
+
+	void Update()
+	{
+		if(MovingRight)
+			transform.position += Vector3.right * 2 * Time.deltaTime;
+		else
+			transform.position += Vector3.left * 2 * Time.deltaTime;
+	
+		if(transform.position.x < XMin)
+		{
+			MovingRight = true;
+			MoveDownStep = true;
+		} 
+		else if(transform.position.x > XMax)
+		{
+			MovingRight = false;
+			MoveDownStep = true;
+		}
+
+		if(MoveDownStep)
+		{
+			transform.position += Vector3.down * 2;
+			MoveDownStep = false;
+		}
+	}
+}
+
+
---
+++ EnemySpawnTester/Assets/Scripts/ShipSpawnPosition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSpawnPosition : MonoBehaviour 
+{
+	[SerializeField]
+	public GameObject ShipPrefab;
+
+	Vector3 TopLeft;
+
+	bool EnemiesSpawned = false;
+
+	bool KeepSpawning = true;
+
+	int SpawnCount = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		TopLeft = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distanceToCamera));
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!EnemiesSpawned)
+		{
+			InvokeRepeating("SpawnNewShip", 1, 1);
+			EnemiesSpawned = true;
+		}
+	}
+
+	void SpawnNewShip()
+	{
+		if(KeepSpawning)
+		{
+			GameObject spawnedShip = (GameObject)Instantiate(ShipPrefab, TopLeft, Quaternion.identity);
+			Enemy enemy = spawnedShip.gameObject.GetComponent<Enemy>();
+			SpawnCount++;
+			KeepSpawning =  SpawnCount < enemy.GetSpawnCount();
+		}
+
+	}
+}
